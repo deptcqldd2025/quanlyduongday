69bf93bf37a61e4d3fa2b4559613f66ed8112950
deptcqldd2025 <deptc.qldd2025@gmail.com>
2026-01-22 03:22:43 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// js/config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBlK2eFfv1Ca-3b_Ew1yXTG3GcWdYw_b5M",
+  authDomain: "baocaongay-e73a2.firebaseapp.com",
+  projectId: "baocaongay-e73a2",
+  storageBucket: "baocaongay-e73a2.firebasestorage.app",
+  messagingSenderId: "75165392007",
+  appId: "1:75165392007:web:4ec22bb1612108b096ac22",
+  measurementId: "G-TEGTD9KFCW"
+};
+
+export const UPLOAD_API = "https://script.google.com/macros/s/AKfycbwanzWLo8PhCuOnTGrP-Ra_5rbYvkCgdgbIr-gSPMxbsXJ-5creXdid0_GYQM6AufT4rA/exec";
